c14012e1684de177390288cf4d74fff92974a62f
Samira Silva <samirapgti@gmail.com>
2024-06-14 13:41:38 +0200

    Add files via upload

---
+++ Scripts_Statistical_Tests/kruskal_wallis_test.r
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+
+#http://www.sthda.com/english/wiki/kruskal-wallis-test-in-r
+
+
+# If .txt tab file, use this
+my_data <- read.delim("data.txt")
+
+#install if it is not installed
+#install.packages("dplyr")
+
+library(dplyr)
+group_by(my_data, group) %>%
+  summarise(
+    count = n(),
+    mean = mean(as.numeric(weight), na.rm = TRUE),
+    sd = sd(as.numeric(weight), na.rm = TRUE),
+    median = median(as.numeric(weight), na.rm = TRUE),
+    IQR = IQR(as.numeric(weight), na.rm = TRUE)
+  )
+
+kruskal.test(weight ~ group, data = my_data)
+
+
